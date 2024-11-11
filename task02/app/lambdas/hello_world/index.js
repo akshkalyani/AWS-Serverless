7aa3f299b6e7dc62bbd332f58cb40b6e94bafeb5
Aksh Kalyani <akshdharmesh_kalyani@gmail.com>
2024-11-11 15:27:36 +0530

    updated url_config and index

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,22 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        message: JSON.stringify('Hello from Lambda'),
-    };
-    return response;
+    const path = event.requestContext.http.path;
+    const method = event.requestContext.http.method;
+ 
+    if (path === '/hello' && method === 'GET') {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                statusCode: 200,
+                message: "Hello from Lambda"
+            }),
+        };
+    } else {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+            }),
+        };
+    }
 };
